e1022f262a03d023124f4d4a70b027e398decd26
kanai1 <ksh30918@kakao.com>
2023-09-27 15:34:34 +0900

    FEAT: 회원가입 페이지 - register.ejs 파일 만들기 - server.js에 라우팅 추가하기

--- server.js
+++ server.js
@@ -9,7 +9,8 @@ app.use(express.json())
 app.use(express.static('public'));
 app.set('views', './public');
 
-app.use('/', (req, res) => {res.render('index')})
+app.get('/', (req, res) => {res.render('index')})
+app.get('/register', (req, res) => {res.render('register')})
 
 app.use((err, req, res, next) => {
 	console.error(err.stack)
