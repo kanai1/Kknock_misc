3ee08ef432219fe7703f1f16794a6fd2f208d774
kanai1 <ksh30918@kakao.com>
2023-09-27 15:22:37 +0900

    FEAT: 500에러 세팅

--- server.js
+++ server.js
@@ -10,4 +10,10 @@ app.use(express.static('public'));
 app.set('views', './public');
 
 app.use('/', (req, res) => {res.render('index')})
+
+app.use((err, req, res, next) => {
+	console.error(err.stack)
+	res.status(500).send("SomeThing Error");
+})
+
 app.listen(process.env.PORT);
