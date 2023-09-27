6a183dcbe28b40d64447f7592d19aea908a3f0da
kanai1 <ksh30918@kakao.com>
2023-09-27 14:51:53 +0900

    Feat: 기초 미들웨어 세팅 - cookie-parser - express.json - ejs view

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
+require('dotenv').config();
 const express = require('express')
+const cookieParser = require('cookie-parser')
 
 const app = express()
+app.set("view engine", "ejs");
+app.use(cookieParser())
+app.use(express.json())
+app.use('/static', express.static('public'));
 
-app.use('/', (req, res) => {res.send('hello')})
-app.listen(8888);
+app.use('/', (req, res) => {res.render('index')})
+app.listen(process.env.PORT);
