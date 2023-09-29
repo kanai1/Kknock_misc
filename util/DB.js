96181574ea103cf62b6226363bb9923f3ce21d6a
kanai1 <ksh30918@kakao.com>
2023-09-29 21:13:32 +0900

    Refactor: mysql서버가 준비될 시간 기다리기 - wait-port모듈과 await를 활용하여 mysql서버가 준비되는 것을 확인한 후 연결시도

--- util/DB.js
+++ util/DB.js
@@ -1,12 +1,29 @@
 require('dotenv').config();
 const mysql = require('mysql2');
-const conn = mysql.createConnection({
+const waitPort = require('wait-port')
+
+const connection = mysql.createConnection({
 	host     : process.env.DB_HOST,
 	user     : process.env.DB_USER,
 	password : process.env.DB_PASSWORD,
 	database : process.env.DB_NAME
-  });
+});
 
-conn.connect();
 
-module.exports = conn;
+(async () => {
+	await waitPort({ host: process.env.DB_HOST, port: 3306 });
+	console.log('MySQL server is ready, connecting...');
+  
+
+	connection.connect((err) => {
+		if (err) {
+			console.error('Database connection error: ', err);
+		} else {
+		  console.log('Connected to the database');
+		  // 여기에서 애플리케이션을 시작할 수 있습니다.
+		  
+		}
+	});
+})()
+
+module.exports = connection
